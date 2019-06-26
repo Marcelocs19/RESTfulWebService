dce3e59004fa30796a6bcbcdc225da148c44fca2
marcelocs19 <marcelo.home19@gmail.com>
2019-06-26 14:23:16 -0300

    Atualização do projeto

--- src/main/java/hello/Application2.java
+++ src/main/java/hello/Application2.java
@@ -6,9 +6,9 @@ import org.slf4j.LoggerFactory;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
-public class Application {
+public class Application2 {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(Application2.class);
 
     public static void main(String args[]) {
         RestTemplate restTemplate = new RestTemplate();
--- src/main/java/hello/QuoteController.java
+++ src/main/java/hello/QuoteController.java
@@ -1,7 +1,9 @@
 package hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class QuoteController {
 
 	@GetMapping("/quotes")
